d19fe7a2d7dc61d0fb14b16c12a553b6038d651c
vitoria maia <vitoria.maia@maiscode.com.br>
2025-04-01 17:06:41 -0300

    edit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,37 +1,132 @@
 import React from "react";
-
-import { useNavigate, useLocation } from "react-router-dom";
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { useLocation, useNavigate } from "react-router-dom"; // Remove useNavigate
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 export const Header: React.FC = () => {
+  const navigation = [
+    { name: 'Dashboard', href: '#', current: true },
+    { name: 'Team', href: '#', current: false },
+    { name: 'Projects', href: '#', current: false },
+    { name: 'Calendar', href: '#', current: false },
+  ];
+
+  function classNames(...classes: (string | undefined | null | false)[]): string {
+    return classes.filter(Boolean).join(' ');
+  }
+
   const navigate = useNavigate();
   const location = useLocation();
   if (location.pathname === "/Register") {
     return null;
   }
-    return (
-      <header style={{ textAlign: "center", padding: "20px", background: "rgb(39 37 37)" }}>
-        <nav className=" shadow-gray-300 w-100 px-8 md:px-auto">
-          <div className="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
-            
-            <div className="text-gray-500 order-3 w-full md:w-auto md:order-2">
-              <ul className="flex font-semibold justify-between">
-                <li className="md:px-4 md:py-2 text-indigo-500"><a href="#">Dashboard</a></li>
-                <li className="md:px-4 md:py-2 hover:text-indigo-400"><a href="#">Search</a></li>
-                <li className="md:px-4 md:py-2 hover:text-indigo-400"><a href="#">Explore</a></li>
-                <li className="md:px-4 md:py-2 hover:text-indigo-400"><a href="#">About</a></li>
-                <li className="md:px-4 md:py-2 hover:text-indigo-400"><a href="#">Contact</a></li>
-              </ul>
-            </div>
-            <div className="order-2 md:order-3">
-              <button onClick={() => navigate("/Register")} className="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
+
+  return (
+    <header className="bg-gray-800"style={{ textAlign: "center", padding: "20px"}}>
+      <nav className="shadow-gray-300 w-100 px-8 md:px-auto">
+        <Disclosure as="nav" >
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button */}
+                <DisclosureButton className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-none focus:ring-inset">
+                  <span className="sr-only">Open main menu</span>
+                  <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-open:hidden" />
+                  <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-open:block" />
+                </DisclosureButton>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        aria-current={item.current ? 'page' : undefined}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium',
+                        )}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                {/* Profile dropdown */}
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-none">
+                      <button
+                        onClick={() => navigate("/Register")}
+                        className="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2"
+                      >
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clip-rule="evenodd" />
+                          <path
+                            fillRule="evenodd"
+                            d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
+                            clipRule="evenodd"
+                          />
                         </svg>
                         <span>Register</span>
-                    </button>
+                      </button>
+                    </MenuButton>
+                  </div>
+                  <MenuItems
+                    transition
+                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-none data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                  >
+                    <MenuItem>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-none"
+                      >
+                        Your Profile
+                      </a>
+                    </MenuItem>
+                    <MenuItem>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-none"
+                      >
+                        Settings
+                      </a>
+                    </MenuItem>
+                    <MenuItem>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-none"
+                      >
+                        Sign out
+                      </a>
+                    </MenuItem>
+                  </MenuItems>
+                </Menu>
+              </div>
             </div>
           </div>
-        </nav>
-      </header>
-      
-    );
-  }
+
+          <DisclosurePanel className="sm:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigation.map((item) => (
+                <DisclosureButton
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  aria-current={item.current ? 'page' : undefined}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium',
+                  )}
+                >
+                  {item.name}
+                </DisclosureButton>
+              ))}
+            </div>
+          </DisclosurePanel>
+        </Disclosure>
+      </nav>
+    </header>
+  );
+};
